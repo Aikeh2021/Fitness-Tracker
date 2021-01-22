29d93d91296f303d943e3f73d691b0e58affb55d
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-01-21 20:51:07 -0500

    Coded the basic 5 step process for setting up an express server.

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`App is running on http://localhost:${PORT}`);
+});
