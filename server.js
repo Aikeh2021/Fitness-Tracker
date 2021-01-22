f34cf35c2e1531c7ab4777719e6e1a40b25147cf
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-01-21 20:55:25 -0500

    Set up an api/config route to test and make sure my server is working correctly.

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//API Test route
+
+app.get("/api/config", (req, res) => {
+    res.send("This is where my page will live.");
+});
+
 app.listen(PORT, () => {
     console.log(`App is running on http://localhost:${PORT}`);
 });
