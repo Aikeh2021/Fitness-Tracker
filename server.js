f450d036dd876efda8e5fc87a54ca62ba33b9415
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-01-21 21:08:54 -0500

    Set up the view for the stats.html page.

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ app.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "public/exercise.html"));
 });
 
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/stats.html"));
+});
+
 app.listen(PORT, () => {
     console.log(`App is running on http://localhost:${PORT}`);
 });
