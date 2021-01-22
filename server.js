1e420e2d6e072339a18d76c04a430ef6529d1e20
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-01-21 21:04:07 -0500

    Index view route is complete and functioning properly.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 //API Test route
 
@@ -17,7 +18,7 @@ app.get("/api/config", (req, res) => {
 //View Routes
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"))
+    res.sendFile(path.join(__dirname, "/index.html"))
 })
 
 app.listen(PORT, () => {
