e0d0f1911c520a999c00d39c856fb7fb9a278c32
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-01-22 22:24:15 -0500

    Required the logger and mongoose packages at the top of the server.js file.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 8080;
 
