31f6c8bd317013fce81b9e506b9d53f9106c6e53
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-01-22 22:26:27 -0500

    Required my model files in the server.js.

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 8080;
 
+const Cardio = require("./models/cardioModel.js");
+const Resistance = require("./models/reistanceModel.js");
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
