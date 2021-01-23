8c7a28eab52bd5fea3bf9121181e8d9e91b2e90d
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-01-22 22:29:52 -0500

    Set up the mongoose connection on the server.

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethoddb", {  useNewUrlParser: true,
+useUnifiedTopology: true,
+useFindAndModify: false,
+useCreateIndex: true  });
+
 //API Test route
 
 app.get("/api/config", (req, res) => {
